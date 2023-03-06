0e778ff65cc2f8201b50a2ef1c1cfb32b54cc111
Giulia <giulia.brugnatti@studio.unibo.it>
2023-03-06 15:58:02 +0100

    perf: improve script automation

--- evaluation/src/main/kotlin/utils/Main.kt
+++ evaluation/src/main/kotlin/utils/Main.kt
@@ -12,8 +12,7 @@ import java.lang.Exception
 @Suppress("NestedBlockDepth")
 fun main(args: Array<String>) {
     val problems = listOf(BlockWorldDomain.Problems.stackAB, LogisticsDomain.Problems.robotFromLoc1ToLoc2)
-    val maxLength = if (args.isNotEmpty()) args[0].toInt() else 50
-    val isWorkflow = args.isNotEmpty()
+    val maxLength = if (args.isNotEmpty()) args[0].toInt() else 5
     val explanationTypes = listOf("C", "")
     fun buildBenchmark() {
         val b = BaseBenchmark()
@@ -23,7 +22,7 @@ fun main(args: Array<String>) {
             for (j in explanationTypes) {
                 for (i in 1..5) {
                     try {
-                        b.writeBenchmark("", problem, j, i, plans, isWorkflow)
+                        b.writeBenchmark("", problem, j, i, plans)
                     } catch (_: Exception) { }
                 }
             }
