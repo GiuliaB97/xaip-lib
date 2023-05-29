a057776ee64a101daaae215ebfddffb7b191ea84
Giulia <giulia.brugnatti@studio.unibo.it>
2023-05-29 22:27:42 +0200

    fix: mbdf fix indentation

--- explanation/src/main/kotlin/explanation/impl/BaseExplanationPresenter.kt
+++ explanation/src/main/kotlin/explanation/impl/BaseExplanationPresenter.kt
@@ -63,13 +63,8 @@ internal open class BaseExplanationPresenter(
 
     private val isPlanValid by lazy {
         "\nThe novel plan is: ${explanation.novelPlan.operators}." +
-            "\nThe novel plan is ${
-                if (!explanation.isPlanValid()) {
-                    "not "
-                } else {
-                    ""
-                }
-            }a valid solution for the problem.\n"
+            "\nThe novel plan is ${not(explanation.isPlanValid())}" +
+            "a valid solution for the problem.\n"
     }
 
     private fun areAllGroundFluents(targets: Set<Fluent>) = targets.all { it.isGround }
